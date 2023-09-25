d1c7b5b56fe4b7ff0203993d511c31e67d25ffcf
Filipe Moreira <fimorei@microsoft.com>
2023-09-25 17:54:23 +0400

    fixing the SQL URL creation using SQLAlchemy

--- application/AzureOpenAIUtil/SqlServer.py
+++ application/AzureOpenAIUtil/SqlServer.py
@@ -3,20 +3,28 @@ from AzureOpenAIUtil.agent.SqlServerAgent import create_sql_agent, SQLDatabaseTo
 from langchain.callbacks.base import CallbackManager
 from langchain.llms.openai import AzureOpenAI
 from langchain.agents import AgentExecutor
+from sqlalchemy.engine.url import URL
 
 class SqlServer:
     cb_handler = HtmlCallbackHandler()
     cb_manager = CallbackManager(handlers=[cb_handler])
 
     def __init__(self, llm, Server, Database, Username, Password, port=1433, odbc_ver=18, topK=10) -> None:
+           
+        db_config = {
+            'drivername': 'mssql+pyodbc',
+            'username': Username,
+            'password': Password,
+            'host': Server,
+            'port': 1433,
+            'database': Database,
+            'query': {'driver': 'ODBC Driver '+ str(odbc_ver) +' for SQL Server'}
+        }
+        #print(db_config)
+        db_url = URL.create(**db_config)
+        #print(db_url)
         
-        odbc_conn = 'Driver={ODBC Driver '+ str(odbc_ver) + ' for SQL Server};Server=tcp:' + \
-            Server + f';Database={Database};Uid={Username};Pwd={Password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-        params = urllib.parse.quote_plus(odbc_conn)
-        self.conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-
-
-        db = SQLDatabase.from_uri(self.conn_str)
+        db = SQLDatabase.from_uri(db_url)
         self.toolkit = SQLDatabaseToolkit(db=db, callback_manager=self.cb_manager)
         # print(deploy_name)
         self.agent_executor = create_sql_agent(llm,
